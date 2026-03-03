b39240fd90be54f32dd409110bcfe24176d3302f
wasp32679 <ryan.kenmoe@outlook.fr>
2026-03-03 12:41:27 +0100

    changed default repo url with my actual repo url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
-import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jobtrek.github.io/', // TODO: Change to your github url
-    base: 'progress-report', // TODO: Change to your repo name
+    site: 'https://github.com/wasp32679', // TODO: Change to your github url
+    base: 'Dossier-de-formation', // TODO: Change to your repo name
     integrations: [starlight({
         title: 'Progress  report',
         social: [
-            { icon: 'github', label: 'GitHub', href: 'https://github.com/jobtrek/progress-report' } // TODO: Change to your repo url
+            { icon: 'github', label: 'GitHub', href: 'https://github.com/wasp32679/Dossier-de-formation' } // TODO: Change to your repo url
         ],
         editLink: {
-            baseUrl: 'https://github.com/jobtrek/progress-report/edit/main/docs/', // TODO: before /edit, change to your repo url
+            baseUrl: 'https://github.com/wasp32679/Dossier-de-formation/edit/main/docs/', // TODO: before /edit, change to your repo url
         },
         customCss: [
             // Relative path to your custom CSS file
