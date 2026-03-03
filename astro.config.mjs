27569672435e238c5f98f65ad236ab06cbcabf00
wasp32679 <ryan.kenmoe@outlook.fr>
2026-03-03 13:17:09 +0100

    renamed repo and updated references

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://github.com/wasp32679', // TODO: Change to your github url
-    base: 'Dossier-de-formation', // TODO: Change to your repo name
+    base: 'progress-report', // TODO: Change to your repo name
     integrations: [starlight({
         title: 'Progress  report',
         social: [
-            { icon: 'github', label: 'GitHub', href: 'https://github.com/wasp32679/Dossier-de-formation' } // TODO: Change to your repo url
+            { icon: 'github', label: 'GitHub', href: 'https://github.com/wasp32679/progress-report' } // TODO: Change to your repo url
         ],
         editLink: {
-            baseUrl: 'https://github.com/wasp32679/Dossier-de-formation/edit/main/docs/', // TODO: before /edit, change to your repo url
+            baseUrl: 'https://github.com/wasp32679/progress-report/edit/main/docs/', // TODO: before /edit, change to your repo url
         },
         customCss: [
             // Relative path to your custom CSS file
